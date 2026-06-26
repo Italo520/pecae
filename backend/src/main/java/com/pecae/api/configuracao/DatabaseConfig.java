b0e62a035750c90854df8d9220951569d74b9367
italo520 <italo520@outlook.com>
2026-06-26 13:41:11 -0300

    Add DatabaseConfig to support postgresql:// and postgres:// PaaS connection strings

---
+++ backend/src/main/java/com/pecae/api/configuracao/DatabaseConfig.java
@@ -0,0 +1,61 @@
+package com.pecae.api.configuracao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String defaultUrl;
+
+    @Value("${spring.datasource.username}")
+    private String defaultUsername;
+
+    @Value("${spring.datasource.password}")
+    private String defaultPassword;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        // Verifica se a URL foi passada no formato PaaS (postgres:// ou postgresql://)
+        String urlToParse = defaultUrl;
+
+        if (urlToParse != null && (urlToParse.startsWith("postgres://") || urlToParse.startsWith("postgresql://"))) {
+            URI dbUri = new URI(urlToParse);
+            
+            String username = defaultUsername;
+            String password = defaultPassword;
+            
+            if (dbUri.getUserInfo() != null) {
+                String[] userInfo = dbUri.getUserInfo().split(":");
+                username = userInfo[0];
+                if (userInfo.length > 1) {
+                    password = userInfo[1];
+                }
+            }
+
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + 
+                           (dbUri.getPort() != -1 ? dbUri.getPort() : 5432) + 
+                           dbUri.getPath() + (dbUri.getQuery() != null ? "?" + dbUri.getQuery() : "");
+
+            return DataSourceBuilder.create()
+                    .url(dbUrl)
+                    .username(username)
+                    .password(password)
+                    .build();
+        }
+
+        // Retorna o padrão configurado
+        return DataSourceBuilder.create()
+                .url(defaultUrl)
+                .username(defaultUsername)
+                .password(defaultPassword)
+                .build();
+    }
+}
